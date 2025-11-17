f169625cadcf96e65ec8e3f3eb93bc478f4cc3e4
Lim, Kenzhi <kenzhilim@gmail.com>
2025-11-17 23:48:14 +0700

    page CreateAccount + CheckEmail
    di CheckEmail tombol navigatenya blm diarahin soalnya routenya blm ada

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import ForgotPassword from "./pages/ForgotPassword";
+import CreateAccount from "./pages/CreateAccount";
+import CheckEmail from "./pages/CheckEmail";
 
 function App() {
   return (
@@ -10,8 +12,9 @@ function App() {
       <Toaster position="top-right" />
 
       <Routes>
+        <Route path="/createaccount" element={<CreateAccount />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
-
+        <Route path="/checkemail" element={<CheckEmail />} />
         <Route path="/" element={<div>Home Page</div>} />
       </Routes>
     </Router>
