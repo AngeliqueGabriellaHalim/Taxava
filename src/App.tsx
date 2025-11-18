b4ef81b810fc4f031c88fd42c445761491a8156f
ievankaa <6182201037@student.unpar.ac.id>
2025-11-18 18:57:48 +0700

    Add route LoginPageEmail and LoginPageUsername pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
+import LoginPageEmail from "./pages/LoginPageEmail";
+import LoginPageUsername from "./pages/LoginPageUsername";
 import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
@@ -10,9 +12,11 @@ function App() {
       <Toaster position="top-right" />
 
       <Routes>
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-
         <Route path="/" element={<div>Home Page</div>} />
+
+        <Route path="/login-email" element={<LoginPageEmail />} />
+        <Route path="/login-username" element={<LoginPageUsername />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
