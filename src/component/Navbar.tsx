9409e5af5cfb7754d4bed7d58723369a23ea6513
angeliquegbrl <angeliquegabriella991@gmail.com>
2025-12-18 00:34:02 +0700

    added design

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="w-full bg-zinc-950 border-b border-zinc-800 px-6 py-4 flex items-center justify-between sticky top-0 z-50">
+    <nav className="w-full bg-zinc-950  px-6 py-4 flex items-center justify-between sticky top-0 z-50 h-[12vh]">
       {/* Logo navigate ke Home */}
       <button
         onClick={handleHome}
@@ -35,16 +35,17 @@ const Navbar: React.FC = () => {
       <div className="flex items-center gap-4 text-zinc-300">
         <button
           onClick={handleHome}
-          className="px-4 py-2 rounded-full border border-zinc-700 text-sm font-semibold hover:bg-zinc-800 transition"
+          className="px-4 py-2 rounded-full border hover:bg-linear-to-r hover:opacity-90
+ hover:from-indigo-600 hover:to-[#7C3AED] border-zinc-700 text-sm font-semibold  transition cursor-pointer"
         >
           Home
         </button>
 
         <button
           onClick={handleLogout}
-          className="px-4 py-2 rounded bg-red-600 hover:bg-red-400 text-sm font-semibold shadow-md transition"
+          className="px-4 py-2 rounded cursor-pointer bg-red-600 hover:bg-red-400 text-sm font-semibold shadow-md transition"
         >
-          Logout
+          Log out
         </button>
       </div>
     </nav>
