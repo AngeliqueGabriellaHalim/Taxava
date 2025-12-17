e3ad68fc4231a0e6130cb05857c7f5e937e5741b
angeliquegbrl <angeliquegabriella991@gmail.com>
2025-12-18 01:04:06 +0700

    aded manage prop design

--- src/pages/ManageProperties.tsx
+++ src/pages/ManageProperties.tsx
@@ -35,7 +35,7 @@ const ManageProperties: React.FC = () => {
   const propertiesOwned = useMemo(() => {
     if (!currentUser) return [];
     return getPropertiesByUser(currentUser.id);
-  }, [currentUser])
+  }, [currentUser]);
 
   // search + sort
   const filteredProperties = useMemo(() => {
@@ -53,7 +53,7 @@ const ManageProperties: React.FC = () => {
   if (!currentUser) {
     return <Navigate to="/login" replace />;
   }
-  
+
   // CLICK HANDLERS
   const handleAdd = () => {
     navigate("/property-setup"); //  pindah ke halaman setup property
@@ -64,10 +64,10 @@ const ManageProperties: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-zinc-900 text-white flex flex-col">
+    <div className="min-h-screen text-white flex flex-col bg-linear-to-r/hsl from-[#191A1F] from-15% to-[#06012F]">
       {/* Navbar */}
       <Navbar />
-      <div className="min-h-screen bg-zinc-900 text-white flex justify-center px-4 py-10">
+      <div className="min-h-screen text-white flex justify-center px-4 py-10 bg-linear-to-r/hsl from-[#191A1F] from-15% to-[#06012F]">
         <div className="absolute top-6 left-6">
           <span className="tracking-[0.3em] text-xl font-semibold">TAXAVA</span>
         </div>
@@ -76,13 +76,9 @@ const ManageProperties: React.FC = () => {
           <h1 className="text-4xl md:text-5xl font-bold mb-2">
             Manage Properties
           </h1>
-          <p className="text-sm text-zinc-400 mb-6">
-            Logged in as{" "}
-            <span className="font-semibold">{currentUser.username}</span>
-          </p>
 
           {/* Search bar */}
-          <div className="mb-8">
+          <div className="mb-8 mt-4">
             <label
               htmlFor="property-search"
               className="block text-sm text-zinc-300 mb-2"
@@ -90,7 +86,7 @@ const ManageProperties: React.FC = () => {
               Search Property
             </label>
 
-            <div className="flex items-center gap-3 bg-zinc-800 rounded-full px-4 py-3 shadow-md">
+            <div className="flex items-center gap-3 bg-slate-800 rounded-full px-4 py-3 shadow-md bg">
               <span className="text-zinc-400 text-xl">
                 <Search />
               </span>
@@ -106,7 +102,7 @@ const ManageProperties: React.FC = () => {
               <button
                 type="button"
                 onClick={() => setSortAsc((prev) => !prev)}
-                className="rounded-full border border-zinc-600 px-3 py-1 text-xs uppercase tracking-wide text-zinc-300 hover:bg-zinc-700 transition"
+                className="rounded-full border border-zinc-600 px-3 py-1 text-xs uppercase tracking-wide text-zinc-300 hover:bg-zinc-700 transition cursor-pointer"
               >
                 {sortAsc ? "A → Z" : "Z → A"}
               </button>
@@ -132,7 +128,7 @@ const ManageProperties: React.FC = () => {
               return (
                 <div
                   key={property.id}
-                  className="bg-zinc-800 rounded-2xl p-4 md:p-5 shadow-lg border border-zinc-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+                  className="bg-zinc-700/40 backdrop-blur-sm  rounded-2xl p-4 md:p-5 shadow-lg border border-zinc-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
                 >
                   <div className="space-y-1">
                     <h3 className="text-2xl font-semibold">{property.name}</h3>
@@ -156,7 +152,7 @@ const ManageProperties: React.FC = () => {
                     <button
                       type="button"
                       onClick={() => handleEdit(property.id)} //  EDIT
-                      className="px-5 py-2 rounded-full bg-indigo-500 hover:bg-indigo-400 text-sm font-semibold shadow-md transition"
+                      className="px-5 py-2 rounded-full bg-indigo-500 hover:bg-indigo-400 text-sm font-semibold shadow-md transition cursor-pointer"
                     >
                       Edit Property
                     </button>
@@ -167,12 +163,12 @@ const ManageProperties: React.FC = () => {
           </div>
 
           {/* Add Property */}
-          <div className="mt-4 mb-10 pb-6 pt-4 border-t border-zinc-800 bg-zinc-900/60 sticky bottom-0">
+          <div className="mt-4 mb-10 pb-6 pt-4 border-t-4 border-zinc-900 bg-linear-to-r/hsl from-[#191A1F] from-5% to-[#06012F] sticky bottom-0">
             <div className="flex justify-center">
               <button
                 type="button"
                 onClick={handleAdd} //  ADD
-                className="px-10 py-3 rounded-full bg-indigo-500 hover:bg-indigo-400 text-base font-semibold shadow-lg transition"
+                className="px-10 py-3 rounded-full bg-linear-to-r from-indigo-600 to-[#7C3AED] hover:from-indigo-400 hover:to-[#8B5CF6]text-base font-semibold shadow-lg transition cursor-pointer"
               >
                 Add Property
               </button>
